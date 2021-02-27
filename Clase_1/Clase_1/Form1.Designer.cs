12ce02d0aec6a73cd827c95a799fa1e2e329d23b
Edgarunam <31267745+Edgarunam@users.noreply.github.com>
2021-02-27 12:54:50 -0600

    Add files via upload

---
+++ Clase_1/Clase_1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace Clase_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Ejecutar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Ejecutar
+            // 
+            this.btn_Ejecutar.Location = new System.Drawing.Point(26, 21);
+            this.btn_Ejecutar.Name = "btn_Ejecutar";
+            this.btn_Ejecutar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Ejecutar.TabIndex = 0;
+            this.btn_Ejecutar.Text = "Ejecutar";
+            this.btn_Ejecutar.UseVisualStyleBackColor = true;
+            this.btn_Ejecutar.Click += new System.EventHandler(this.btn_Ejecutar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(148, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_Ejecutar);
+            this.Name = "Form1";
+            this.Text = "Pbox_salida";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Ejecutar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Clase_1/Clase_1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Ejecutar_Click(object sender, EventArgs e)
+        {
+            Graphics Papel;
+            Papel = pictureBox1.CreateGraphics();
+            Pen lapiz = new Pen(Color.Black);
+           
+            
+
+            for(int i=0; i<pictureBox1.Width/2; i= i+5)
+            {
+                Papel.DrawLine(lapiz, 0, 0+i, 0+i, pictureBox1.Height/2);
+            }
+
+            for (int j = 0; j < pictureBox1.Height/2; j = j + 5)
+            {
+                Papel.DrawLine(lapiz, (pictureBox1.Width-j)/2, (pictureBox1.Height)/2, 0, (pictureBox1.Height-j)/2);
+                
+            }
+
+
+        }
+    }
+}
