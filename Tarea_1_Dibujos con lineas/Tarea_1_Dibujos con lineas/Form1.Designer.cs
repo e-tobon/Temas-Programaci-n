964cebf39dcd2310aaa6a2d84e1a01c64e6dc579
Edgarunam <31267745+Edgarunam@users.noreply.github.com>
2021-03-01 19:08:48 -0600

    Add files via upload

---
+++ Tarea_1_Dibujos con lineas/Tarea_1_Dibujos con lineas/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace Tarea_1_Dibujos_con_lineas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox2.Location = new System.Drawing.Point(235, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox3.Location = new System.Drawing.Point(453, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox4.Location = new System.Drawing.Point(672, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox5.Location = new System.Drawing.Point(12, 230);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox6.Location = new System.Drawing.Point(235, 230);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox7.Location = new System.Drawing.Point(453, 230);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox8.Location = new System.Drawing.Point(672, 230);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 454);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 31);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Dibujar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 512);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Tarea_1_Dibujos con lineas/Tarea_1_Dibujos con lineas/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea_1_Dibujos_con_lineas
+{
+    public partial class Form1 : Form
+    {
+        Graphics papel;
+        Pen lapiz = new Pen(Color.Black);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Dibujo_1(papel, lapiz);
+            Dibujo_2(papel, lapiz);
+            Dibujo_3(papel, lapiz);
+            Dibujo_4(papel, lapiz);
+            Dibujo_5(papel, lapiz);
+            Dibujo_6(papel, lapiz);
+            Dibujo_7(papel, lapiz);
+            Dibujo_8(papel, lapiz);
+        }
+
+        private void Dibujo_1(Graphics Papel, Pen lapiz)
+        {
+            Papel = pictureBox1.CreateGraphics();
+            for (int i = 0; i < pictureBox1.Height; i = i + 8)
+            {
+                Papel.DrawLine(lapiz, 0, i, pictureBox1.Width, i);
+
+            }
+
+        }
+
+        private void Dibujo_2(Graphics Papel, Pen lapiz)
+        {
+            Papel = pictureBox2.CreateGraphics();
+
+            for(int i = 0; i < pictureBox2.Width; i = i + 5)
+            {
+                Papel.DrawLine(lapiz, i, 0, i, pictureBox2.Height);
+            }
+        }
+
+        private void Dibujo_3(Graphics Papel, Pen lapiz)
+        {
+            Papel = pictureBox3.CreateGraphics();
+            for(int i = 0; i<pictureBox1.Height; i = i+ 5)
+            {
+                Papel.DrawLine(lapiz, 0, i, pictureBox3.Width, i);
+            }
+
+            for(int j = 0; j<pictureBox3.Width; j = j + 5)
+            {
+                Papel.DrawLine(lapiz,j,0,j,pictureBox3.Height);
+            }
+        }
+
+        private void Dibujo_4(Graphics Papel, Pen lapiz)
+        {
+            Papel = pictureBox4.CreateGraphics();
+            for(int i = 0; i < pictureBox4.Width; i = i + 8)
+            {
+                Papel.DrawLine(lapiz, i, 0, pictureBox4.Width / 2, pictureBox4.Height / 2);
+                Papel.DrawLine(lapiz, i, pictureBox4.Height, pictureBox4.Width / 2, pictureBox4.Height / 2);
+               
+            }
+        }
+
+        private void Dibujo_5(Graphics Papel, Pen lapiz)
+        {
+            Papel = pictureBox5.CreateGraphics();
+            for(int i=0; i<pictureBox5.Height; i = i + 5)
+            {
+                Papel.DrawLine(lapiz, 0, i, i, 0);
+            }
+            for (int j = 0; j <= pictureBox5.Height; j = j + 5)
+            {
+                Papel.DrawLine(lapiz, pictureBox5.Width, pictureBox5.Height-j, pictureBox5.Width-j, pictureBox5.Height);
+            }
+        }
+
+        private void Dibujo_6(Graphics Papel, Pen Lapiz)
+        {
+            Papel = pictureBox6.CreateGraphics();
+            for(int i = 0; i< pictureBox6.Width; i = i + 5)
+            {
+                Papel.DrawLine(Lapiz, pictureBox6.Width, i, i, 0);
+                
+            }
+        }
+        
+        private void Dibujo_7(Graphics Papel, Pen Lapiz)
+        {
+            Papel = pictureBox7.CreateGraphics();
+            for (int i = 0; i < pictureBox7.Width; i = i + 5)
+            {
+                
+                Papel.DrawLine(Lapiz, pictureBox7.Width, i, i, 0);
+                Papel.DrawLine(Lapiz, 0,i, pictureBox7.Width - i, 0);
+                Papel.DrawLine(Lapiz, 0, pictureBox7.Height - i, pictureBox7.Width - i, pictureBox7.Height);
+                Papel.DrawLine(Lapiz, pictureBox7.Width, i, pictureBox7.Width-i, pictureBox7.Height);
+
+            }
+
+        }
+
+
+
+        private void Dibujo_8(Graphics Papel, Pen Lapiz)
+        {
+            Papel = pictureBox8.CreateGraphics();
+            for(int i = 0; i < pictureBox8.Height; i = i + 5)
+            {
+                Papel.DrawLine(Lapiz, 0, i, pictureBox8.Width / 2, pictureBox8.Height / 2);
+                Papel.DrawLine(Lapiz, pictureBox8.Width, i, pictureBox8.Width / 2, pictureBox8.Height / 2);
+            }
+        }
+
+
+    }
+}
